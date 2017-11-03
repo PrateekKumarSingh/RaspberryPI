866b03b0b6558261fe2a16e5f1d9f51505a4941c
Prateek Singh <Prateeksingh1590@gmail.com>
2017-11-03 19:57:29 +0530

    Update Install_Setup_VSCode.sh

--- Install_Setup_VSCode.sh
+++ Install_Setup_VSCode.sh
@@ -19,3 +19,34 @@ Add to product.json:
       "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
       "itemUrl": "https://marketplace.visualstudio.com/items"
       }
+
+
+
+#upgrade
+#   upgrade is used to install the newest versions of all packages
+#   currently installed on the system from the sources enumerated in
+#   /etc/apt/sources.list. Packages currently installed with new
+#   versions available are retrieved and upgraded; under no
+#   circumstances are currently installed packages removed, or packages
+#   not already installed retrieved and installed. New versions of
+#   currently installed packages that cannot be upgraded without
+#   changing the install status of another package will be left at
+#   their current version. An update must be performed first so that
+#   apt-get knows that new versions of packages are available.
+#
+#dist-upgrade
+#   dist-upgrade in addition to performing the function of upgrade,
+#   also intelligently handles changing dependencies with new versions
+#   of packages; apt-get has a "smart" conflict resolution system, and
+#   it will attempt to upgrade the most important packages at the
+#   expense of less important ones if necessary. So, dist-upgrade
+#   command may remove some packages. The /etc/apt/sources.list file
+#   contains a list of locations from which to retrieve desired package
+#   files. See also apt_preferences(5) for a mechanism for overriding
+#   the general settings for individual packages.
+#And with the newer apt tool available from 14.04 onwards:
+#
+#full-upgrade
+#   full-upgrade performs the function of upgrade but may also remove
+#   installed packages if that is required in order to resolve a
+#   package conflict.
